bdea1929eaff2540232d7f7fdfd7ca9c53da0478
Pete Capra <petey.caps@gmail.com>
2013-05-22 07:10:13 +1000

    Changed internal model to be a Vector[Cell]

--- src/main/scala/com/petecapra/sudoku/SudokuMap.scala
+++ src/main/scala/com/petecapra/sudoku/SudokuMap.scala
@@ -1,11 +1,49 @@
 package com.petecapra.sudoku
 
+case class Cell(index: Int, candidates: Set[Int]) {
 
-case class SudokuMap(numbers: Vector[Vector[Int]]) {
+  def solved: Boolean = candidates.size == 1
+  def number: Int = candidates.head
 
+  override def toString = if (solved) number.toString else " "
+
+}
+
+case class SudokuMap(numbers: Vector[Cell]) {
+
+  /**
+   * Helper functions to provide appropriate vectors containing rows, columns and boxes
+   */
+  private def numbersByCol: Vector[Vector[Cell]] = numbers.groupBy( _.index % 9).values.toVector
+  private def numbersByBox: Vector[Vector[Cell]] = 
+    numbers.groupBy( cell => (( cell.index / 27) * 3) + ( cell.index % 9 / 3) ).values.toVector
+  private def numbersByRow: Vector[Vector[Cell]] = numbers.grouped(9).toVector
+ 
+  def solved: Boolean = {
+    def containsAllNumbers: (Vector[Cell] => Boolean) = { 
+      numbers => 1 to 9 forall ( i => numbers.exists( cell => cell.solved && cell.number == 1) ) 
+    }
+    numbersByRow.forall(containsAllNumbers) &&
+    numbersByCol.forall(containsAllNumbers) &&
+    numbersByBox.forall(containsAllNumbers)
+  }
+  
+  /**
+   * print out the numbers in the table in a nice, neat format that's easy to read
+   **/
   override def toString:String = {
-    val horizontalDivider = "-------------------------\n"
-    horizontalDivider
+    (for {
+      index <- 0 to 80
+    } yield {
+       index match {
+         case i if (i % 27 == 0) => "-------------------------\n| " + numbers(index)
+         case 80 => " " + numbers(index) + " |\n-------------------------"
+         case i if (i % 9 == 8) => " " + numbers(index) + " |\n"
+         case i if (i % 3 == 2) => " " + numbers(index) + " |"
+         case i if (i % 9 == 0) => "| " + numbers(index)
+         case _ => " " + numbers(index)
+       } 
+    }).mkString
   }
 
 }
@@ -13,13 +51,14 @@ case class SudokuMap(numbers: Vector[Vector[Int]]) {
 object SudokuMap {
 
   def apply(s: String): SudokuMap = {
-    SudokuMap(s.split("\n").map { row =>
-      row.map { cell => cell match {
-        case ' ' => 0
-        case _ => cell.getNumericValue
-      }
-      }.toVector
-    }.toVector)
+    val numberString = s.replaceAll("\n", "")
+    val numbers = (for { 
+      i <- 0 to 80
+    } yield {
+      val number = numberString(i) 
+      if (number == ' ') Cell(i, Set(1,2,3,4,5,6,7,8,9)) else Cell(i, Set(number.getNumericValue))
+    }).toVector
+    SudokuMap(numbers)
   }
 
 }
--- src/test/scala/com/petecapra/sudoku/SudokuMapTest.scala
+++ src/test/scala/com/petecapra/sudoku/SudokuMapTest.scala
@@ -25,15 +25,22 @@ class SudokuMapTest extends FunSuite {
 
     assert(sudokuMap.numbers ===
       Vector(
-        Vector(1, 2, 3, 4, 5, 6, 7, 8, 9),
-        Vector(1, 2, 3, 4, 5, 6, 7, 8, 9),
-        Vector(1, 2, 3, 4, 5, 6, 7, 8, 9),
-        Vector(1, 2, 3, 4, 5, 6, 7, 8, 9),
-        Vector(2, 2, 3, 4, 5, 1, 7, 8, 9),
-        Vector(1, 2, 3, 4, 5, 6, 7, 8, 9),
-        Vector(1, 2, 3, 0, 5, 6, 7, 8, 9),
-        Vector(1, 2, 3, 4, 5, 6, 7, 8, 9),
-        Vector(1, 2, 3, 4, 5, 6, 7, 8, 9)))
+        Cell(0, Set(1)), Cell(1, Set(2)), Cell(2, Set(3)), Cell(3, Set(4)), Cell(4, Set(5)), Cell(5, Set(6)), 
+        Cell(6, Set(7)), Cell(7, Set(8)), Cell(8, Set(9)), Cell(9, Set(1)), Cell(10, Set(2)), Cell(11, Set(3)),
+        Cell(12, Set(4)), Cell(13, Set(5)), Cell(14, Set(6)), Cell(15, Set(7)), Cell(16, Set(8)),
+        Cell(17, Set(9)), Cell(18, Set(1)), Cell(19, Set(2)), Cell(20, Set(3)), Cell(21, Set(4)), 
+        Cell(22, Set(5)), Cell(23, Set(6)), Cell(24, Set(7)), Cell(25, Set(8)), Cell(26, Set(9)),
+        Cell(27, Set(1)), Cell(28, Set(2)), Cell(29, Set(3)), Cell(30, Set(4)), Cell(31, Set(5)), 
+        Cell(32, Set(6)), Cell(33, Set(7)), Cell(34, Set(8)), Cell(35, Set(9)), Cell(36, Set(2)),
+        Cell(37, Set(2)), Cell(38, Set(3)), Cell(39, Set(4)), Cell(40, Set(5)), Cell(41, Set(1)),
+        Cell(42, Set(7)), Cell(43, Set(8)), Cell(44, Set(9)), Cell(45, Set(1)), Cell(46, Set(2)),
+        Cell(47, Set(3)), Cell(48, Set(4)), Cell(49, Set(5)), Cell(50, Set(6)), Cell(51, Set(7)),
+        Cell(52, Set(8)), Cell(53, Set(9)), Cell(54, Set(1)), Cell(55, Set(2)), Cell(56, Set(3)), 
+        Cell(57, Set(1,2,3,4,5,6,7,8,9)), Cell(58, Set(5)), Cell(59, Set(6)), Cell(60, Set(7)),
+        Cell(61, Set(8)), Cell(62, Set(9)), Cell(63, Set(1)), Cell(64, Set(2)), Cell(65, Set(3)), 
+        Cell(66, Set(4)), Cell(67, Set(5)), Cell(68, Set(6)), Cell(69, Set(7)), Cell(70, Set(8)), 
+        Cell(71, Set(9)), Cell(72, Set(1)), Cell(73, Set(2)), Cell(74, Set(3)), Cell(75, Set(4)), 
+        Cell(76, Set(5)), Cell(77, Set(6)), Cell(78, Set(7)), Cell(79, Set(8)), Cell(80, Set(9))))
 
   }
 
@@ -52,20 +59,39 @@ class SudokuMapTest extends FunSuite {
 
     val toString = SudokuMap(numbers).toString
 
+    println(toString)
+
     assert(toString ===
-      """-------------------------
+     """|-------------------------
         || 1 2 3 | 4 5 6 | 7 8 9 |
         || 1 2 3 | 4 5 6 | 7 8 9 |
         || 1 2 3 | 4 5 6 | 7 8 9 |
         |-------------------------
         || 1 2 3 | 4 5 6 | 7 8 9 |
-        || 2 2 3 | 4   6 | 7 8 9 |
+        || 2 2 3 | 4 5 1 | 7 8 9 |
         || 1 2 3 | 4 5 6 | 7 8 9 |
         |-------------------------
-        || 1 2 3 | 4 5 6 | 7 8 9 |
+        || 1 2 3 |   5 6 | 7 8 9 |
         || 1 2 3 | 4 5 6 | 7 8 9 |
         || 1 2 3 | 4 5 6 | 7 8 9 |
         |-------------------------""".stripMargin)
+
+  }
+
+  test("map solved") {
+
+    val numbers = 
+      """145327698
+        |839654127
+        |672918543
+        |496185372
+        |218473956
+        |753296481
+        |367542819
+        |984761235
+        |521839764""".stripMargin
+
+    assert( SudokuMap(numbers).solved === true )
 
   }
 
